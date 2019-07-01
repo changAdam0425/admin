548c08a907408218790492b1a75d2600b93ee598
changAdam0425 <18235644369@163.com>
2019-07-01 18:05:59 +0800

    基本发布流程

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -5,21 +5,26 @@
       <span>发布文章</span>
     </div>
     <el-form ref="form"
-             :model="form"
+             :model="articleForm"
              label-width="80px">
       <el-form-item label="标题">
-        <el-input></el-input>
+        <el-input v-model="articleForm.title"></el-input>
       </el-form-item>
       <el-form-item label="内容">
-        <el-input></el-input>
+        <el-input type="textarea"
+                  v-model="articleForm.content"></el-input>
       </el-form-item>
       <el-form-item label="封面">
       </el-form-item>
       <el-form-item label="频道">
+        <el-select v-model="articleForm.channel_id">
+          <el-option label="区域一"
+                     value="上海"></el-option>
+        </el-select>
       </el-form-item>
-      <el-form-item label="频道">
-        <el-button>发布</el-button>
-        <el-button>存入草稿</el-button>
+      <el-form-item>
+        <el-button @click="handlePublish(false)">发布</el-button>
+        <el-button @click="handlePublish(true)">存入草稿</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -28,6 +33,41 @@
 
 <script>
 export default {
+  data () {
+    return {
+      articleForm: {
+        title: '', // 标题
+        content: '', // 内容
+        cover: { // 封面
+          type: 0, // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          images: [] // 图片链接
+        },
+        channel_id: '3' // 频道
+      },
+
+    }
+  },
+
+  methods: {
+    handlePublish (draft = false) {
+      this.$http({
+        methods: 'POST',
+        url: '/articles',
+        data: this.articleForm,
+        params: {
+          draft
+        }
+      }).then(data => {
+        this.$message({
+          type: 'success',
+          message: '发布成功'
+        })
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
+
 
 }
 </script>
